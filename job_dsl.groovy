2abb838565778f497588571ea362957593a69893
Marc <marc.mendia@epitech.eu>
2024-03-11 16:20:40 +0100

    [ADD] SEED jobs

--- job_dsl.groovy
+++ job_dsl.groovy
@@ -22,3 +22,19 @@ job('Tools/clone-repository') {
         manualTrigger()
     }
 }
+
+job('Tools/SEED') {
+    description('Seed job for creating new jobs.')
+    parameters {
+        stringParam('GITHUB_NAME', '', 'GitHub repository owner/repo_name (e.g.: "EpitechIT31000/chocolatine')
+        stringParam('DISPLAY_NAME', '', 'Display name for the job')
+    }
+    steps {
+        dsl {
+            external('job_dsl.groovy')
+        }
+    }
+    triggers {
+        manualTrigger()
+    }
+}
